84a47e490ffbed370f97f65d92c5a605413b60ad
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2020-09-27 15:32:59 +0530

    Add files via upload

---
+++ conditiional statements.py
@@ -0,0 +1,38 @@
+                             IF CONDITION
+
+a=int(input('enter a number'))
+if a>50:
+    print(a)
+
+
+
+
+
+
+                          IF ELSE CONDITION
+
+mid="vara@123"
+pwd="1234"
+umid=input('enter a mail id')
+upwd=input('enter a password')
+if umid==mid and upwd==pwd:
+    print('login succesfully')
+else:
+    print('wrong credentials')
+
+
+
+
+
+                               ELIF CONDITION
+
+a=int(input('enter a student details'))
+if a>=91:
+    print('A GRADE')
+elif a>=61 and a<=90:
+    print('B GRADE')
+elif a>=45 and a<=60:
+    print('C GRADE')
+else:
+    print('FAIL')
+    
