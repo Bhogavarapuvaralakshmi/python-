59b911cef6622c3bc95e1b1692e54be7286f9fbe
Bhogavarapuvaralakshmi <71434589+Bhogavarapuvaralakshmi@users.noreply.github.com>
2020-09-28 15:23:39 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,39 @@
+i=0
+while True:
+    print('good morning')
+    i+=1
+    if i==5:
+        break
+
+
+
+
+
+i=0
+while True:
+    print(i)
+    i+=1
+    if i==5:
+        break
+
+
+
+
+
+i=0
+while True:
+    print(i,end=" ")
+    i+=1
+    if i==5:
+        break
+
+
+
+
+
+i=5
+while True:
+    print(i,end=" ")
+    i-=1
+    if i==0:
+        break
